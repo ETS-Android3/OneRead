9bce757e09d207b0de04b2d2b522ef75f015e2dc
tiennhot23@gmail.com <tiennhot23@gmail.com>
2022-01-15 01:21:21 +0700

    dark mode

--- app/src/main/java/com/example/oneread/MainActivity.java
+++ app/src/main/java/com/example/oneread/MainActivity.java
@@ -1,13 +1,103 @@
 package com.example.oneread;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatDelegate;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.google.android.material.navigation.NavigationView;
+import com.makeramen.roundedimageview.RoundedImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    @BindView(R.id.navigation_view)
+    public NavigationView navigationView;
+
+    private LinearLayout headerMenu;
+    private RoundedImageView img_avatar;
+    private TextView txt_edit_profile;
+    private Switch btn_dark_mode;
+
+
+    int NightMode;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        ButterKnife.bind(this);
+
+        //region navigation menu
+        navigationView.setNavigationItemSelectedListener(this);
+        btn_dark_mode = (Switch) navigationView.getMenu().findItem(R.id.dark_mode).getActionView();
+        headerMenu = navigationView.getHeaderView(0).findViewById(R.id.header_menu);
+        img_avatar = headerMenu.findViewById(R.id.img_avatar);
+        txt_edit_profile = headerMenu.findViewById(R.id.txt_edit_profile);
+        img_avatar.setOnClickListener(this);
+        txt_edit_profile.setOnClickListener(this);
+        btn_dark_mode.setOnClickListener(this);
+        //endregion
+
+        //region night mode
+        sharedPreferences = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
+        NightMode = sharedPreferences.getInt("NightModeInt", 1);
+        AppCompatDelegate.setDefaultNightMode(NightMode);
+        if (NightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            btn_dark_mode.setChecked(true);
+        } else btn_dark_mode.setChecked(false);
+        //endregion
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == txt_edit_profile || view == img_avatar) {
+            return;
+        }
+        if (view == btn_dark_mode) {
+            switchToDarkMode();
+            return;
+        }
+    }
+
+    private void switchToDarkMode() {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        NightMode = AppCompatDelegate.getDefaultNightMode();
+
+        sharedPreferences = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        editor.putInt("NightModeInt", NightMode);
+        editor.apply();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about_me: {
+                Toast.makeText(this, "Not Available", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        return false;
     }
 }
